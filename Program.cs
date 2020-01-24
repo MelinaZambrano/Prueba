4008ca1e7ab529703b5bcd960e65afea229aa476
MelinaZambrano <58448090+MelinaZambrano@users.noreply.github.com>
2020-01-24 16:30:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pruebaSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = Singleton.GetInstance();
+            var singleton2 = Singleton.GetInstance();
+            var singleton3 = Singleton.GetInstance();
+            var singleton4 = Singleton.GetInstance();
+
+            singleton1.Name = "Sample 1";
+            singleton2.Name = "Sample 2";
+            singleton3.Name = "Sample 3";
+            singleton4.Name = "Sample 4";
+
+            Console.WriteLine("Objeto 1: " + singleton1.Name);
+            Console.WriteLine("Objeto 2: " + singleton2.Name);
+            Console.WriteLine("Objeto 3: " + singleton3.Name);
+            Console.WriteLine("Objeto 4: " + singleton4.Name);
+        }
+    }
+
+}
+
---
+++ Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pruebaSP
+{
+    class Singleton
+    {
+
+        public string Name { get; set; }
+        
+            private Singleton()
+            {
+
+            }
+
+            private static Singleton _singleton;
+
+            public static Singleton GetInstance()
+            {
+
+                if (_singleton == null)
+                {
+
+                    _singleton = new Singleton();
+
+                }
+
+                return _singleton;
+
+            }
+
+    }
+    
+}
